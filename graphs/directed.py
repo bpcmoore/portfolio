36719d6af91d9a05b9e3f1a5c8030878b1861756
bpcmoore <brookpcm@gmail.com>
2024-03-20 10:37:50 -0700

    added remove_edge, get_vertices, and get_edges to directed graph

--- graphs/directed.py
+++ graphs/directed.py
@@ -16,6 +16,20 @@ class Directed:
         if source < len(self.mat) or dest < len(self.mat):
             self.mat[source][dest] = 0   
 
+    def get_vertices(self):
+        v = []
+        for vertices in range(len(self.mat)):
+            v.append(vertices)
+        return v
+
+    def get_edges(self):
+        edges = []
+        for v in range(len(self.mat)):
+            for e in range(len(self.mat[v])):
+                if self.mat[v][e] != 0:
+                    edges.append((v, e, self.mat[v][e]))
+        return edges
+
 
 if __name__ == "__main__":
     graph = Directed()
@@ -25,5 +39,6 @@ if __name__ == "__main__":
         (3, 1, 5), (2, 1, 23), (3, 2, 7)]
     for src, dst, weight in edges:
         graph.add_edge(src, dst, weight)
-    graph.remove_edge(0, 9, 4)
+    graph.remove_edge(0, 1)
     print(graph.mat)
+    print(graph.get_edges())
