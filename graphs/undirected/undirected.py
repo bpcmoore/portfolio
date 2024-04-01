864a9d9c4919136ba4b6ea701358d5c52980f402
bpcmoore <brookpcm@gmail.com>
2024-04-01 16:32:46 -0700

    Added comments to undirected graph and updated formatting to conform with flake8

--- graphs/undirected/undirected.py
+++ graphs/undirected/undirected.py
@@ -1,5 +1,9 @@
+# Author: Brook Moore
+# Last Update: 04/01/2024
 
 class Undirected:
+    '''Undirected class initiates graph as an adjacency list and provides
+    methods for altering the graph and getting information about the graph'''
     def __init__(self, edges=None):
         self.adjacency_list = dict()
         if edges is not None:
@@ -7,34 +11,42 @@ class Undirected:
                 self.add_edge(e[0], e[1])
 
     def add_vertex(self, name: str):
+        '''Adds a new vertex of given name to adjacency list.'''
         if name in self.adjacency_list:
             return
         self.adjacency_list[name] = []
 
     def add_edge(self, v1: str, v2: str):
+        '''Adds an edge between two vertices of given name. If either given
+        vertex is in the adjacency list, method automatically adds it.'''
         self.add_vertex(v1)
         self.add_vertex(v2)
         self.adjacency_list[v1].append(v2)
         self.adjacency_list[v2].append(v1)
 
     def remove_edge(self, v1: str, v2: str):
-        if v1 in self.adjacency_list[v2]:
-            self.adjacency_list[v1].remove(v2)
-            self.adjacency_list[v2].remove(v1)
+        '''Removes an edge between two given vertices.'''
+        if v1 in self.adjacency_list and v2 in self.adjacency_list:
+            if v1 in self.adjacency_list[v2]:
+                self.adjacency_list[v1].remove(v2)
+                self.adjacency_list[v2].remove(v1)
 
     def remove_vertex(self, name: str):
+        '''Removes given vertex and all edges associated with vertex.'''
         if name in self.adjacency_list:
             for v in self.adjacency_list:
                 self.remove_edge(name, v)
             del self.adjacency_list[name]
 
     def get_vertices(self):
+        '''Returns a list of all vertices in the adjacency list.'''
         vertices = []
         for v in self.adjacency_list:
             vertices.append(v)
         return vertices
 
     def get_edges(self):
+        '''Returns a list of all edges in the adjacency list.'''
         edges = []
         for v in self.adjacency_list:
             for e in self.adjacency_list[v]:
@@ -45,6 +57,8 @@ class Undirected:
         return edges
 
     def valid_path(self, path: str):
+        '''Takes a string consisting of vertices as a parameter. Returns True
+        if the string of vertices is a valid path.'''
         for index in range(len(path) - 1):
             if path[index] in self.adjacency_list:
                 if path[index + 1] in self.adjacency_list[path[index]]:
@@ -52,8 +66,10 @@ class Undirected:
                 else:
                     return False
         return True
-    
+
     def dfs(self, v1, v2=None):
+        '''Does a depth first search on graph. Requires the starting vertex as
+        a parameter but can also take the destination vertex.'''
         stack = [v1]
         dfs = [v1]
         while len(stack) > 0:
@@ -67,8 +83,10 @@ class Undirected:
                     dfs.append(e)
                     break
         return dfs
-           
+
     def bfs(self, v1, v2=None):
+        '''Does a breadth first search on graph. Requires the starting vertex
+        as a parameter but can also take the destination vertex.'''
         stack = [v1]
         bfs = [v1]
         while len(stack) > 0:
@@ -81,6 +99,7 @@ class Undirected:
                     bfs.append(e)
         return bfs
 
+
 if __name__ == "__main__":
     edges = ['AE', 'AC', 'BE', 'CE', 'CD', 'CB', 'BD', 'ED', 'BH', 'QG', 'FG']
     g = Undirected(edges)
@@ -91,3 +110,7 @@ if __name__ == "__main__":
     for i in range(1, len(test_cases)):
         v1, v2 = test_cases[i], test_cases[-1 - i]
         print(f'{v1}-{v2} DFS:{g.dfs(v1, v2)} BFS:{g.bfs(v1, v2)}')
+    g = Undirected(['AB', 'AC', 'BC', 'BD', 'CD', 'CE', 'DE'])
+    test_cases = ['ABC', 'ADE', 'ECABDCBE', 'ACDECB', '', 'D', 'Z']
+    for path in test_cases:
+        print(list(path), g.valid_path(list(path)))
